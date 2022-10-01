fb3abd9d752e44d32fa709439b5be34194568aab
tserede <kasy.06@mail.ru>
2022-10-01 12:58:57 +0300

    удаление файлов

--- JS/Lesson 1/index.js
+++ JS/Lesson 1/index.js
@@ -1,45 +1,76 @@
-/*console.log("Hello,world!");
+// /*console.log("Hello,world!");
 
-const ourPlanetName = "Земля";
+// const ourPlanetName = "Земля";
 
-console.log(ourPlanetName);
+// console.log(ourPlanetName);
 
-let time = "12:10";
+// let time = "12:10";
 
-console.log(time);
+// console.log(time);
 
-let NumberOfArticles = "15";
+// let NumberOfArticles = "15";
 
-console.log(NumberOfArticles);
+// console.log(NumberOfArticles);
 
-let Payment = "Наличными";
+// let Payment = "Наличными";
 
-console.log(Payment);
+// console.log(Payment);
 
-let UserName = "Таня";
+// let UserName = "Таня";
 
-console.log(UserName);
+// console.log(UserName);
 
-const name="IVAN";
-const age="46";
-const userData=`Имя пользователя: ${name}, возраст ${age}`;
-const userDataNew="Имя пользователя: ${name}, возраст ${age}";
+// const name="IVAN";
+// const age="46";
+// const userData=`Имя пользователя: ${name}, возраст ${age}`;
+// const userDataNew="Имя пользователя: ${name}, возраст ${age}";
 
-console.log(userData);
+// console.log(userData);
 
-let isAdmin=false;
-let isOpen=true;
+// let isAdmin=false;
+// let isOpen=true;
 
-let townName;
+// let townName;
 
-console.log(townName);
+// console.log(townName);
 
-townName="Moskow";
+// townName="Moskow";
 
-console.log(townName);*/
+// console.log(townName);
 
-const userData = {
-  name: "Ivan",
-  age: 34,
-};
-console.log(typeof userData.name);
+// const userData = {
+//   name: "Ivan",
+//   age: 34,
+// };
+// console.log(typeof userData.name);
+
+// const  name ="alex";
+
+// const nameToNumber = Number(name);
+
+// console.log(nameToNumber);
+// const age=26;
+// const ageToString=String(age);
+// const ageToString=String(age);
+// console.log(ageToString);
+
+
+console.log(Boolean(""));
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean({})); 
+
+console.log(typeof +"45");
+console.log(typeof Number("45"));
+console.log(typeof (2 + 5));
+
+// console.log(2 == 3);
+// console.log(2 == 2);
+// console.log(2 + "3");
+// console.log("2" + 3);
+// console.log("2" + "3");
+// console.log(2 + 3);
+// console.log("2" + 2 + 3);
+// console.log (typeof ("3"*"1"));
+// console.log (typeof ("3"+"1"));
+
