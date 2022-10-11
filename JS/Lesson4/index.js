698e1cfbfb02f4be24c0280932389559fc7c984a
tserede <kasy.06@mail.ru>
2022-10-11 23:35:31 +0300

    Изменен файл

--- JS/Lesson4/index.js
+++ JS/Lesson4/index.js
@@ -50,17 +50,47 @@
 //      else {
 //       console.log(c);
 //     } конец задачи
-const t = 40;
-if (t <= -30) {
-  console.log("Оставайтесь дома!");
-} else if (t > -30 && t <= -10) {
-  console.log("Сегодня холодно");
-} else if (t > -10 && t <= 5) {
-  console.log("Не холодно");
-} else if (t>5&&t<=15) {
-    confirm.log ("Тепло");
-} else if (t>15&&t<=25) {
-    console.log("Очень тепло");
-} else {
-    console.log ("Пекло");
+// const t = 40;
+// if (t <= -30) {
+//   console.log("Оставайтесь дома!");
+// } else if (t > -30 && t <= -10) {
+//   console.log("Сегодня холодно");
+// } else if (t > -10 && t <= 5) {
+//   console.log("Не холодно");
+// } else if (t>5&&t<=15) {
+//     confirm.log ("Тепло");
+// } else if (t>15&&t<=25) {
+//     console.log("Очень тепло");
+// } else {
+//     console.log ("Пекло");
+// }
+// let i=0;
+// while (i<100){
+//   console.log (i);
+//   i=i+1;
+
+// }
+
+// let i = 0;
+// while (true) {
+//   if(i<30){
+//     i += 1;
+//   } else {
+//     break;
+//   }
+//    console.log(i);
+// }
+let str="Строка";
+i=1;
+N=4;
+// for (i=0;i<N;i++) {
+//   console.log(str);
+// }
+while (true){
+  if(i<=N){
+    i+=1
+  } else {
+    break;
+  }
+  console.log(str);
 }
