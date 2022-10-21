7025fbe526cf39a4d9bbf50b28d8ac060c515235
tserede <kasy.06@mail.ru>
2022-10-21 22:04:00 +0300

    создан файл с домашней работой

--- JS/Lesson4/Homework/hw4.js
+++ JS/Lesson4/Homework/hw4.js
@@ -9,7 +9,7 @@
 // else {
 //     console.log("a самый большой");
 // }
-
+/*Задача 1*/
 // let a=-3
 // if(a == 0){
 //   console.log("Верно");
@@ -107,5 +107,154 @@
 //   console.log("Верно");
 // } else {console.log ("Неверно");
 // }
-let a="fsfs";
-console.log(a);
+/*На switch-case*/
+// let num = 5;
+// switch (num) {
+//   case 1:
+//     result = "Зима";
+//   case 2:
+//     result = "Весна";
+//   case 3:
+//     result = "Лето";
+//   case 4:
+//     result = "Осень";
+//     default:
+//         result="Нет таких значений";
+// }
+// console.log(result);
+// /*Общие задачи */
+// /*Задачи 1*/
+// let day = 200;
+// if (day >= 1 && day <= 10) {
+//   console.log("Первая декада месяца");
+// } else if (day > 10 && day <= 20) {
+//   console.log("Вторая декада месяца");
+// } else if (day > 20 && day < +31) {
+//   console.log("Третья декада месяца");
+// } else {
+//   console.log("Значение указано неверно");
+// }
+// /* Задача 2*/
+// let month=12
+// if (month==1||month==2||month==12){
+//     console.log("Зима");
+// } else if(month>=3&&month<=5){
+//     console.log("Весна");
+// } else if (month>=6&&month<=8){
+//     console.log("Лето");
+// }else if(month>=9&& month<=11){
+//     console.log("Осень");
+// } else {
+//     console.log("Значение не верное");
+// }
+// /*Задача 3*/
+// let str = "abcde";
+// if (str[0] == "a") {
+//   console.log("Да");
+// } else {
+//   console.log("Нет");
+// }
+// /* Задача 4*/
+// let str="12345";
+// if(str[0]>=1&&str[0]<=3){
+//     console.log("Да");
+// } else {console.log ("Нет");
+// }
+// /*Задача 5*/
+// let str="458";
+// a=Number(str[0]); b=Number(str[1]); c=Number(str[2]);
+// console.log(sum=a+b+c);
+// /*Задача 6*/
+// let str="474487";
+// let sum1=Number(str[0])+Number(str[1])+Number(str[2]);
+// let sum2=Number(str[3])+Number(str[4])+Number(str[5]);
+// if (sum1==sum2){
+//     console.log ("Да");
+// } else {console.log ("Нет");}
+/*Циклы while и for*/
+// /*Задача 1*/
+// let a=1;
+// while(a<101){
+//     console.log(a);
+//     a=a+1;
+// }
+// for(let i=1; i<=100;i++){
+//     console.log(i);
+// }
+// /*Задача 2*/
+// let a=11;
+// while (a<=33){
+//     console.log(a);
+//     a=a+1;
+// }
+// for(let i=11;i<=33; i++){
+//     console.log(i);
+// }
+// /*Задача 3*/
+// let a=0
+// while(a<=100){
+//     if(a%2==0){
+//             console.log(a);
+//     }
+//         a=a+1;
+//     }
+// for(let a=0; a<=100; a++){
+//     if(a%2==0){
+//         console.log(a);
+//     }
+// }
+// /*Задача 4*/
+// let a=1;
+// let sum=0;
+// while(a<=100){
+//     sum=sum+a;
+//     a=a+1;
+// }
+// console.log(sum);
+// let sum=0;
+// for(a=1;a<=100;a++){
+//    sm=sum+a;
+// }
+// console.log(sum);
+/*Работа с for для массивов */
+// /*Задача 1*/
+// let arr=[1,2,3,4,5];
+
+// for (let i=0; i<arr.length;i++){
+// console.log(arr[i]);
+// }
+/* Задача 2*/
+// let arr=[1,2,3,4,5];
+// let sum=0
+// for(let a=0;a<arr.length; a++){
+//     sum=sum+arr[a];
+// }
+// console.log(sum);
+/*Задачи общие*/
+// /*Задача 1*/
+// let arr=[2,5,9,15,0,4];
+// for(let i=0; i<arr.length;i++){
+//     if(arr[i]>3&&arr[i]<10){
+//         console.log(arr[i]);
+//     }
+// }
+// /*Звдача 2*/
+// let arr=[7,7,-3,5,3,-1,-9,5];
+// let sum=0
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]>=0){
+//         sum=sum+arr[i];
+
+//     }
+// } console.log(sum);
+
+// /*Задача 3*/
+// let arr = [1, 2, 5, 9, 4, 13, 4, 10];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 4) {
+//     console.log("Есть!");
+//     break;
+//   }
+// }
+/*Задача 4*/
+let arr=[10,20,30,50,235,3000];
