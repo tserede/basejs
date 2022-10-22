ce07312806d1fac150f9a3c0b640fa14dba4ba29
tserede <kasy.06@mail.ru>
2022-10-22 10:10:08 +0300

    изменение файла

--- JS/Lesson6/Classwork/lesson6.js
+++ JS/Lesson6/Classwork/lesson6.js
@@ -1,2 +1,10 @@
-let a=5;
-console.log(a);
+const key="FirstName";
+const person={
+[key]:"Alex";
+};
+person.age=56;
+person['city-name']="NN";
+person[4]=4;
+console.log(person.FirstName);
+console.log(person["city-name"]);
+
