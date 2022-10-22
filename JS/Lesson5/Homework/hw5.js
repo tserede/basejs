f948d9ed1ff4e7d183f36e4e5e0911dde4c4b179
tserede <kasy.06@mail.ru>
2022-10-22 08:49:21 +0300

    создан файл lesson5

--- JS/Lesson5/Homework/hw5.js
+++ JS/Lesson5/Homework/hw5.js
@@ -1,2 +1,62 @@
-let a=5;
-console.log(a);
+// /*Задача 1*/
+// function quadro(a,b=2){
+//     return a**b;
+// }
+// console.log(quadro(4));
+// /*Задача 2*/
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum(3,5));
+// /*Задача 3*/
+// function result(a,b,c){
+//     return (a-b)/c;
+// }
+// console.log(result(24,9,5));
+// /*Задача 4*/
+// function dayWeek(day){
+//     // let day=5;
+//     if (day==1){
+//         console.log("Понедельник");
+//     }else if(day==2){
+//         console.log("Вторник");
+// }else if(day==3){
+//     console.log("Среда");
+// }else if(day==4){
+//     console.log("Четверг");
+// }else if(day==5){
+//     console.log("Пятница");
+// }else if(day==6){
+//     console.log("Суббота");
+// } else if(day==7){
+//     console.log("Воскресенье");
+// } }
+// dayWeek(4);
+// /*Задача 5*/
+// function fun(a,b){
+//     if (a===b){console.log("true");}
+//     else { console.log("false");}
+// }
+// fun(5,5);
+// /*Задача 6*/
+// function fun(a,b){
+//     if((a+b)>10){
+//         console.log("true");
+//     } else console.log("false");
+// }
+// fun(6,5);
+// /*Задача 7*/
+// function num(a){
+//     if(a<0){ console.log("true");
+// }else console.log("false");
+// }
+// num(-6);
+/*Задача 8*/
+function isNumberRange(a) {
+  if (a > 0 && a < 10) {
+    return true;
+  } else return false;
+}
+console.log(isNumberRange(5));
+let arr=[5,76,44,9,2,7,9];
+
