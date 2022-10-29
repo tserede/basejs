a6b8068128c1b0e87e5143b80af1850d37d60f15
tserede <kasy.06@mail.ru>
2022-10-29 09:56:06 +0300

    Файл изменен

--- JS/Lesson6/Homework/hw6.js
+++ JS/Lesson6/Homework/hw6.js
@@ -22,16 +22,76 @@
 //    console.log(str[0].toUpperCase()+str.substr(1,str.length));
 //}
 // StructStr("слово");
-// Задача 5
-const capitalize=(str)=>{
-    arr=str.split(" ");
-    let arrowStr=[];
-    for(let i=0;i<arr.lenght;i++){
-       a=arr[i]
-       strS[0].toUpperCase()+str.substr(1,str.length
-//     arrowStr+=a;
-//  }
+// // Задача 5
+// const capitalize=(str)=>{
+//     arr=str.split(" ");
+//     let arrowStr=[];
+//     for(let i=0;i<arr.lenght;i++){
+//        a=arr[i]
+//        strS[0].toUpperCase()+str.substr(1,str.length
+// //     arrowStr+=a;
+// //  }
 
- console.log(arr);
+//  console.log(arr);
+// };
+// capitalize("Белеет парус одинокий");
+// //7
+// const charArr=[".",",","-","/","!","&","?","*"];
+// const removeChar=(str)=>{
+//     for(let i=0; i<charArr.length; i++){
+//         while(str.includes(charArr[i])){
+//           str=str.replace(charArr[i],"");
+//         }
+//         }
+//         let result="";
+//         for(let i=0; i<str.length;i++){
+//             if(str[i])==""&&str[i-1]!==""){
+//                 result+=str[i];
+//             }
+//             if(str[i]!=""){
+//                 result+=str[i];
+//             }
+//         }
+//     return str;
+//     }
+// console.log(removeChar("vjjsd7&@%?fsffgvs.,"));
+//var2
+// const removeChar=(str)=>{
+//     let result="";
+//     for(let i=0; i<str.length;i++){
+//         if(/[a-zA-Z]|[а-яА-Я]|[0-9]|\s/.test(str[i])){
+//             result +=str[i];
+//         }
+//     }
+//     return result;
+// };
+// console.log(removeChar("vjjsd7&@%?fsffgvs.,"));
+// 12
+// const camelCase="CurrentUserSecondName";
+// const initSnake=(str)=>{
+//     let result="";
+//     for(let i=0; i<str.lenght; i++){
+//         if(str[i].toUpperCase===str[i]&&i!==0){
+//             result+=`_${str[i].toLowerCase()}`;
+//         } else{
+//             result+=str[i].toLowerCase();
+//         }
+//     }
+//     return result;
+// }
+// console.log(initSnake("CurrentUserSecondName"));
+//16
+const getSubstr=(str,char,pos)=>{
+    if(str.includes(char)){
+if(pos==="before"){
+return str.slice(0,str.indexOf(char));
+}
+if(pos==="after"){
+    return str.slice(str.indexOf(char));
+
+
+}
+    }
+    return `Символ ${char} отсутствует в строке ${str}`;
 };
-capitalize("Белеет парус одинокий");
+console.log(getSubstr("My name Alex!", "b", "after"));
