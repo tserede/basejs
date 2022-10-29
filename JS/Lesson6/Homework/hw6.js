4e20a6d875be4473af9d44275aca807337f199d9
tserede <kasy.06@mail.ru>
2022-10-29 08:53:27 +0300

    Файл изменен

---
+++ JS/Lesson6/Homework/hw6.js
@@ -0,0 +1,36 @@
+// //Задача 1
+//const stringToarroy=(str)=>{
+//   arr=str.split(" ");
+//     console.log(arr);
+//}
+// stringToarroy("Буря мглою небо кроет");
+// //Задача 2
+// const deleteCharacters=(str,length)=>{
+//     console.log(str.slice(0,length));
+// }
+// deleteCharacters("Белеет парус одинокий",5);
+// // Задача 3
+// const insertDash=(str)=>{
+//     str=str.toUpperCase();
+//     arr=str.split(" ");
+//     console.log(arr.join("-"));
+     
+// };
+// insertDash("Отворите мне темницу");
+// // Задача 4
+//const StructStr=(str)=>{
+//    console.log(str[0].toUpperCase()+str.substr(1,str.length));
+//}
+// StructStr("слово");
+// Задача 5
+const capitalize=(str)=>{
+    arr=str.split(" ");
+//  let arrowStr=[];
+//  for(let i=0;i<stringToarroy(str)[i];i++){
+//     let a=StructStr(stringToarroy(str)[i]);
+//     arrowStr+=a;
+//  }
+
+ console.log(arr);
+};
+capitalize("Белеет парус одинокий");
