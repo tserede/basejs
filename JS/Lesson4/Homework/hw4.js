8c8ed9aced34ffc79190a927d917c43a8f9e244a
tserede <kasy.06@mail.ru>
2022-11-04 17:31:52 +0300

    файл изменен

--- JS/Lesson4/Homework/hw4.js
+++ JS/Lesson4/Homework/hw4.js
@@ -260,7 +260,7 @@
 let arr=[10,20,30,50,235,3000];
 for(let i=0;i<arr.length; i++){
     let str=String(arr[i]);
-    if(str[0]===1||str[0]===5||str[0]===2){
+    if(str[0]==1||str[0]==5||str[0]==2){
         console.log(str[0]);
     }
 }
