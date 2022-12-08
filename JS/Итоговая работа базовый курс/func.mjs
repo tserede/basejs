9fdc1dd3f675b62272d41d8f5b1ebbbde9da7ad3
tserede <kasy.06@mail.ru>
2022-12-08 14:38:51 +0300

    Файл изменен

---
+++ JS/Итоговая работа базовый курс/func.mjs
@@ -0,0 +1,37 @@
+
+const createList=()=>{
+    const itemArr=["Купить игрушки", "Нарядить елку","Повесить гирлянду"];
+    for(let val of itemArr){
+        const listItem=document.createElement("li");
+        listItem.textContent=val;
+        list.append(listItem);
+        listItem.className="li"
+        console.log(listItem.textContent);
+    }
+};
+// createList();
+// const themeBtn=document.getElementById("theme_button");
+//  const themeToggle=function(){
+//     const body=document.body;
+//     body.classList.toggle("light_theme");
+//     if(body.classList.contains("light_theme")){
+//         themeBtn.textContent="Темная тема";
+//     } else{
+//         themeBtn.textContent="Светлая тема";
+//     }
+    
+//  };
+//  themeBtn.addEventListener("click",themeToggle);
+
+//  const btnAdd=document.getElementById("buttonAdd");
+//  function createListItem(){
+//     const newListItem=document.createElement("li");
+//     newListItem.className="li";
+//     newListItem.textContent=input.value;
+//     list.append(newListItem)
+    
+//  }
+//  btnAdd.addEventListener("click",createListItem)
+ 
+
+export {createList}
--- JS/Итоговая работа базовый курс/index.mjs
+++ JS/Итоговая работа базовый курс/index.mjs
@@ -1,3 +1,4 @@
+import * as ScriptModule from "./func.mjs";
 const body = document.body;
 console.log(body);
 
@@ -51,6 +52,7 @@ const buttonAdd=document.createElement("button");
 wrapperAdd.append(buttonAdd);
 buttonAdd.textContent="Добавить"
 buttonAdd.className = "buttonAdd";
+buttonAdd.id="buttonAdd";
 buttonAdd.classList.add("btn");
             //Список
 const list=document.createElement("ul")
@@ -63,19 +65,10 @@ const wrapperListItem=document.createElement("wrapper");
 list.append(wrapperListItem);
 wrapperListItem.className = "wrapperListItem";
 
-const createList=()=>{
-    const itemArr=["Купить игрушки", "Нарядить елку","Повесить гирлянду"];
-    for(let val of itemArr){
-        const listItem=document.createElement("li");
-        listItem.textContent=val;
-        wrapperListItem.append(listItem);
-        listItem.className="li";
-        listItem.style.fontSize="25px"
-        listItem.style.margin="15px"
-        console.log(listItem.textContent);
-    }
-};
-createList();
+
+
+scriptModule.createList();
+console.log(scriptModule.createList());
 
 const themeBtn=document.getElementById("theme_button");
  const themeToggle=function(){
@@ -89,6 +82,17 @@ const themeBtn=document.getElementById("theme_button");
     
  };
  themeBtn.addEventListener("click",themeToggle);
+
+ const btnAdd=document.getElementById("buttonAdd");
+ function createListItem(){
+    const newListItem=document.createElement("li");
+    newListItem.className="li";
+    newListItem.textContent=input.value;
+    list.append(newListItem)
+    
+ }
+ btnAdd.addEventListener("click",createListItem)
+ 
 
 
 //                     //Элемент 1
