b010c02986a5564d3b92412673c0ed2e0ab054da
tserede <kasy.06@mail.ru>
2022-12-09 11:37:22 +0300

    файл изменен

--- JS/todolist/func.js
+++ JS/todolist/func.js
@@ -1,28 +1,22 @@
-
- export const createList=()=>{
-    const itemArr=["Купить игрушки", "Нарядить елку","Повесить гирлянду"];
-    for(let val of itemArr){
-        const listItem=document.createElement("li");
-        const list=document.getElementById("list")
-        listItem.textContent=val;
-        list.append(listItem);
-        listItem.className="li"
-        console.log(listItem.textContent);
-    }
+export const createList = () => {
+  const itemArr = ["Купить игрушки", "Нарядить елку", "Повесить гирлянду"];
+  for (let val of itemArr) {
+    const listItem = document.createElement("li");
+    const list = document.getElementById("list");
+    listItem.textContent = val;
+    list.append(listItem);
+    listItem.className = "li";
+    console.log(listItem.textContent);
+  }
 };
 
-export const themeToggle=function(){
-    const themeBtn=document.getElementById("theme_button");
-    const body=document.body;
-    body.classList.toggle("light_theme");
-    if(body.classList.contains("light_theme")){
-        themeBtn.textContent="Темная тема";
-    } else{
-        themeBtn.textContent="Светлая тема";
-    }
-    
- };
-
-
- 
-
+export const themeToggle = function () {
+  const themeBtn = document.getElementById("theme_button");
+  const body = document.body;
+  body.classList.toggle("light_theme");
+  if (body.classList.contains("light_theme")) {
+    themeBtn.textContent = "Темная тема";
+  } else {
+    themeBtn.textContent = "Светлая тема";
+  }
+};
--- JS/todolist/index.js
+++ JS/todolist/index.js
@@ -1,4 +1,4 @@
-import { createList, themeToggle} from "./func.js";
+import { createList, themeToggle } from "./func.js";
 const body = document.body;
 console.log(body);
 
@@ -7,111 +7,77 @@ const wrapper = document.createElement("div");
 body.append(wrapper);
 wrapper.className = "wrapper";
 
-
-    // header
+// header
 const newHeader = document.createElement("header");
 newHeader.className = "header";
 wrapper.append(newHeader);
 
-
-        // Заголовок
+// Заголовок
 const newTitle = document.createElement("h1");
 newHeader.append(newTitle);
 newTitle.textContent = "TO DO LIST";
 newTitle.style.color = "red";
 
-    // Кнопка
-const buttonOne= document.createElement("button");
+// Кнопка темы
+const buttonOne = document.createElement("button");
 wrapper.append(buttonOne);
 buttonOne.className = "themeButton";
-buttonOne.id="theme_button"
+buttonOne.id = "theme_button";
 buttonOne.classList.add("btn");
 buttonOne.textContent = "Темная тема";
 
-    // Контейнер
-const section=document.createElement("section");
+// Контейнер
+const section = document.createElement("section");
 wrapper.append(section);
 section.className = "container";
 
-        // Контент
-const content=document.createElement("content");
+// Контент
+const content = document.createElement("content");
 section.append(content);
 section.className = "content";
-            //Обертка формы добавления задачи
-const wrapperAdd=document.createElement("wrapperAdd");
+
+//Обертка формы добавления задачи
+const wrapperAdd = document.createElement("wrapperAdd");
 content.append(wrapperAdd);
 wrapperAdd.className = "wrapperAdd";
-                // Поле ввода задачи
-const input=document.createElement("input");
+
+// Поле ввода задачи
+const input = document.createElement("input");
 wrapperAdd.append(input);
-input.id="input";
-input.type="text";
-input.className="input";
+input.id = "input";
+input.type = "text";
+input.className = "input";
 
-
-                // Кнопка добавления задачи
-const buttonAdd=document.createElement("button");
+// Кнопка добавления задачи
+const buttonAdd = document.createElement("button");
 wrapperAdd.append(buttonAdd);
-buttonAdd.textContent="Добавить"
+buttonAdd.textContent = "Добавить";
 buttonAdd.className = "buttonAdd";
-buttonAdd.id="buttonAdd";
+buttonAdd.id = "buttonAdd";
 buttonAdd.classList.add("btn");
 
-            //Список
-const list=document.createElement("ul")
+//Список
+const list = document.createElement("ul");
 content.append(list);
 list.className = "list";
-list.id="list";
+list.id = "list";
 
-
-                //Обертка элемента списка
-const wrapperListItem=document.createElement("wrapper");
+//Обертка элемента списка
+const wrapperListItem = document.createElement("wrapper");
 list.append(wrapperListItem);
 wrapperListItem.className = "wrapperListItem";
 
-
 createList();
-const themeBtn=document.getElementById("theme_button");
-themeBtn.addEventListener("click",themeToggle);
+const themeBtn = document.getElementById("theme_button");
+themeBtn.addEventListener("click", themeToggle);
 
-
- const btnAdd=document.getElementById("buttonAdd");
- function createListItem(){
-    const newListItem=document.createElement("li");
-    newListItem.className="li";
-    newListItem.textContent=input.value;
-    list.append(newListItem)
-    
- }
- btnAdd.addEventListener("click",createListItem)
- 
-
-// const blockDelete=document.createElement("div");
-// wrapperListItem.append(blockDelete);
-// blockDelete.className="blockDelete";
-
-// const imageDelete=document.createElement("p");
-// blockDelete.append(imageDelete);
-// imageDelete.className="imageDelete";
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const btnAdd = document.getElementById("buttonAdd");
+function createListItem() {
+  const newListItem = document.createElement("li");
+  newListItem.className = "li";
+  newListItem.textContent = input.value;
+  list.append(newListItem);
+}
+btnAdd.addEventListener("click", createListItem);
 
 
